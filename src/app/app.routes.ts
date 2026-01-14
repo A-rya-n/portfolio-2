2e54b5177aba0a33fd15f6a56498e57925696e6f
Aryan Purohit <aryan31.purohit@gmail.com>
2026-01-14 10:01:33 +0530

    routed home page to app root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from '../home/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+    title: 'Aryan Purohit | Home',
+  },
+];
